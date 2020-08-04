81fdee326107fbc4475264697c089052cca2a45e
sushiz <sushiz@users.noreply.github.com>
2020-08-04 20:54:18 +0800

    Project complete albeit a bit slow...

--- app/src/main/java/com/example/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.guessthecelebrity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,16 +18,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> celebURLs, celebNames;
+    ImageView imageView;
+    int randomCeleb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DownloadWebContent dl = new DownloadWebContent();
+        imageView = findViewById(R.id.imageView);
         String result="";
         try {
             result = dl.execute("https://web.archive.org/web/20200118162958/http://www.posh24.se/kandisar").get();
@@ -35,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         Pattern p = Pattern.compile(" alt=\"(.*?)\"");
         Matcher m = p.matcher(split[1]);
-        ArrayList<String> celebNames = new ArrayList<String>();
+        celebNames = new ArrayList<String>();
         int i=0;
         while(m.find()){
             celebNames.add(m.group(1));
@@ -44,12 +55,29 @@ public class MainActivity extends AppCompatActivity {
         }
         Pattern pat = Pattern.compile("img src=\"(.*?)\"");
         Matcher mat = pat.matcher(split[1]);
-        ArrayList<String> celebURLs = new ArrayList<String>();
+        celebURLs = new ArrayList<String>();
         i=0;
         while(mat.find()){
             celebURLs.add(mat.group(1));
             Log.i("URL",celebURLs.get(i));
             i++;
+        }
+        generateQuiz();
+    }
+
+    public class DownloadImage extends  AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try{
+                URL url = new URL(strings[0]);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = httpURLConnection.getInputStream();
+                return BitmapFactory.decodeStream(in);
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
         }
     }
 
@@ -76,5 +104,47 @@ public class MainActivity extends AppCompatActivity {
                 return "failed";
             }
         }
+    }
+
+    public void generateQuiz(){
+        Random r = new Random();
+        randomCeleb = r.nextInt(celebURLs.size());
+        String celebUrl = celebURLs.get(randomCeleb);
+
+        DownloadImage dl = new DownloadImage();
+        try {
+            Bitmap image = dl.execute(celebUrl).get();
+            imageView.setImageBitmap(image);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        int randomAnswer = r.nextInt(4);
+        String[] answers = new String[4];
+        for(int i=0; i<4; i++){
+            answers[i] = celebNames.get(r.nextInt(celebNames.size()));
+        }
+        answers[randomAnswer] = celebNames.get(randomCeleb);
+        button1.setText(answers[0]);
+        button2.setText(answers[1]);
+        button3.setText(answers[2]);
+        button4.setText(answers[3]);
+    }
+
+    public void checkAnswer(View view){
+        Button button = (Button) view;
+        String answer = button.getText().toString();
+        Log.i("selected answer", answer);
+        if(answer.equalsIgnoreCase(celebNames.get(randomCeleb))){
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT).show();
+            Log.i("Right", answer);
+        }else{
+            Toast.makeText(getApplicationContext(), "Wrong!", Toast.LENGTH_SHORT).show();
+            Log.i("Wrong", answer);
+        }
+        generateQuiz();
     }
 }
